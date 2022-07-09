a3e4954fa4c76ad0738412c3653ffca9943fc5c2
Gaylord Kiliopa <gaylordkiliopa@gmail.com>
2022-07-09 07:43:20 -0500

    modify files

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const port = process.env.PORT || 5000
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
 
 // Listen on port 5000
 app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
