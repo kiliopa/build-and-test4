03f1559f6bf42a1c45f15db372a4c217c8fdb407
Gaylord Kiliopa <gaylordkiliopa@gmail.com>
2022-07-09 07:21:42 -0500

    modify files

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const app = express()
 const port = process.env.PORT || 5000
-
-// Listen on port 5000
-app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
